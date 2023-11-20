aa8babe7a174eb970877b0b6364139d8951c8c68
chrisnastro <cnastro@icloud.com>
2023-11-20 15:49:21 -0500

    added getItems for all hours

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ $(document).ready(function () {
   })
 })
 
+$("hour-9 .description").val(localStorage.getItem("hour-9"));
+$("hour-10 .description").val(localStorage.getItem("hour-10"));
+$("hour-11 .description").val(localStorage.getItem("hour-11"));
+$("hour-12 .description").val(localStorage.getItem("hour-12"));
+$("hour-13 .description").val(localStorage.getItem("hour-13"));
+$("hour-14 .description").val(localStorage.getItem("hour-14"));
+$("hour-15 .description").val(localStorage.getItem("hour-15"));
+$("hour-16 .description").val(localStorage.getItem("hour-16"));
+$("hour-17 .description").val(localStorage.getItem("hour-17"));
+
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
