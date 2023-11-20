c95f2f3f0f97560dca25e467d572707c506d6a8a
chrisnastro <cnastro@icloud.com>
2023-11-20 11:41:23 -0500

    added current date in header

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var currentDay = $('#currentDay');
+
+function displayDay() {
+  var rightNow = dayjs().format('MMM DD, YYYY');
+  currentDay.text(rightNow);
+}
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +28,5 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+displayDay();
